6ca6ed436ff5a96f39a3af3689d4d3a4bf5c37ec
Sumner Evans <me@sumnerevans.com>
2022-02-23 22:08:17 -0700

    shell: update to have mbc

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import <nixpkgs> { };
 
   # CoC Config
   cocConfig = pkgs.writeText "coc-settings.json" (
@@ -7,36 +7,43 @@ let
       "texlab.path" = "${pkgs.texlab}/bin/texlab";
     }
   );
+
+  PROJECT_ROOT = builtins.getEnv "PWD";
 in
-  with pkgs;
-  mkShell {
-    # https://e.printstacktrace.blog/merging-json-files-recursively-in-the-command-line/
-    shellHook = ''
-      mkdir -p .vim
-      ln -sf ${cocConfig} .vim/coc-settings.json
-    '';
+with pkgs;
+mkShell {
+  name = "impurePythonEnv";
+  venvDir = "./.venv";
 
-    buildInputs = [
-      # Core
-      gnumake
-      neovim
-      ripgrep
-      rnix-lsp
+  # https://e.printstacktrace.blog/merging-json-files-recursively-in-the-command-line/
+  postShellHook = ''
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
 
-      # LaTeX
-      python38Packages.pygments
-      texlive.combined.scheme-full
+    mkdir -p .vim
+    ln -sf ${cocConfig} ${PROJECT_ROOT}/.vim/coc-settings.json
 
-      # Python
-      (
-        python38.withPackages (
-          ps: with ps; [
-            black
-            flake8
-            jedi
-            neovim
-          ]
-        )
-      )
-    ];
-  }
+    # Add /bin to path
+    export PATH="${PROJECT_ROOT}/bin:$PATH"
+  '';
+
+  buildInputs = [
+    python3
+    python3Packages.venvShellHook
+
+    # Core
+    gnumake
+    neovim
+    ripgrep
+    rnix-lsp
+
+    # LaTeX
+    python3Packages.pygments
+    texlive.combined.scheme-full
+  ];
+
+  # Run this command, only after creating the virtual environment
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+  '';
+}
